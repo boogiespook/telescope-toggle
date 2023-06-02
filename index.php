8acec141aaa06b7a44c026fdb11e9ab74eb1048c
Chris Jenkins <chrisj@redhat.com>
2023-06-02 14:42:43 +0100

    Updated to show where integrations exist

--- index.php
+++ index.php
@@ -15,6 +15,52 @@
     <body>
 <?php
 
+class DotEnv
+{
+    /**
+     * The directory where the .env file can be located.
+     *
+     * @var string
+     */
+    protected $path;
+
+
+    public function __construct(string $path)
+    {
+        if(!file_exists($path)) {
+            throw new \InvalidArgumentException(sprintf('%s does not exist', $path));
+        }
+        $this->path = $path;
+    }
+
+    public function load() :void
+    {
+        if (!is_readable($this->path)) {
+            throw new \RuntimeException(sprintf('%s file is not readable', $this->path));
+        }
+
+        $lines = file($this->path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+        foreach ($lines as $line) {
+
+            if (strpos(trim($line), '#') === 0) {
+                continue;
+            }
+
+            list($name, $value) = explode('=', $line, 2);
+            $name = trim($name);
+            $value = trim($value);
+
+            if (!array_key_exists($name, $_SERVER) && !array_key_exists($name, $_ENV)) {
+                putenv(sprintf('%s=%s', $name, $value));
+               $_ENV[$name] = $value;
+                $_SERVER[$name] = $value;
+            }
+        }
+    }
+}
+
+(new DotEnv(__DIR__ . '/.env'))->load();
+
 $pg_host = getenv('PG_HOST');
 $pg_db = getenv('PG_DATABASE');
 $pg_user = getenv('PG_USER');
@@ -113,6 +159,18 @@ if ($row['flag'] == "green") {
 	$checked = "";
 }
 
+## Check if there is an integration for the capability
+$integrationQuery = "select count(*) as total from integrations where capability_id = '" . $row['id'] . "'";
+#print $integrationQuery;
+$integrationResult = pg_query($integrationQuery) or die('Error message: ' . pg_last_error());
+$intCount = pg_fetch_assoc($integrationResult);
+#$intCount['total'];
+if ($intCount['total'] > 0) {
+$toggleClass = "toggle-capability-integration";
+} else {
+$toggleClass = "toggle-capability";
+}
+
 print '          	<div class="toggle-label"> 
 <label class="pf-c-switch" for="' . $row['id'] . '">
   <input class="pf-c-switch__input" type="checkbox" name="capability-' . $row['id'] . '" id="' . $row['id'] . '" aria-labelledby="' . $row['id'] . '-on" ' . $checked . ' />
@@ -121,7 +179,7 @@ print '          	<div class="toggle-label">
       <i class="fas fa-check" aria-hidden="true"></i>
     </span>
   </span>
-  <p class="toggle-capability">' . $row['capability'] . '</p>
+  <p class="' . $toggleClass . '">' . $row['capability'] . '</p>
   </div>
 </label>';
 }          
@@ -227,6 +285,7 @@ print "</div></div></div>";
 
 <form id="toggle" class="pf-c-form" action="updateToggle.php" >
     <p class="pf-c-title pf-m-3xl">Telescope Toggle</p>
+    <p>Items in <span class="red">RED</span> indicate that an integration is in place for that capability</p>
       <div class="pf-l-gallery pf-m-gutter">
 
 <!-- CHANGE TO GET DYNAMIC NAMES -->
